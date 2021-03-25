d2173bd7b8f48752a6daf4c52f3efad45d74e4c7
Emiliano Jaime <emilianojaime123@gmail.com>
2021-03-25 16:32:28 -0300

    ej 4 done

---
+++ JavaScript/ej4.js
@@ -0,0 +1,31 @@
+// If Else
+
+// a)
+
+if (Math.random() >= 0.5){
+    console.log('Greater than 0.5');
+ }else{
+    console.log('Lower than 0.5') ;
+ }
+ 
+ // b)
+ 
+ var age = 85; 
+ 
+ if (age < 2){
+    console.log('La persona es un Bebe');
+ } else if ((age >= 2) && (age <= 12)) {
+    console.log('La persona es un Nino');
+ } else if ((age > 12) && (age <= 19)) {
+    console.log('La persona es un Adolecente') 
+ } else if ((age > 19) && (age <= 30)) {
+    console.log('La persona es un Joven')
+ } else if ((age > 30) && (age <= 60)) {
+    console.log('La persona es un Adulto')
+ } else if ((age > 60) && (age <= 75)) {
+    console.log('La persona es un Adulto mayor')
+ } else { 
+    console.log('La persona es un Anciano')
+ }
+ 
+ console.log('Fin Ejercicio 4 , comienzo Ejercicio 5 --------------------------------------');
