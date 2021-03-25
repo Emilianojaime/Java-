b1f3af0b53cf04a52d46d494ac5ebfcbc701934a
Emiliano Jaime <emilianojaime123@gmail.com>
2021-03-25 08:41:23 -0300

    exercise 1 done

--- JavaScript/ej1.js
+++ JavaScript/ej1.js
@@ -1,1 +1,27 @@
-console.log('testing console')
+// Variables y operadores
+
+// a)
+
+var x = 1;
+var y = 2;
+var z = x + y; 
+console.log('El numero resultante de sumar', x ,'+', y ,'es', z );
+
+// b)
+
+var names = 'Emiliano';
+var lastname = 'Jaime';
+var full_name = names + lastname;
+
+console.log('El resultante de sumar', name ,'+', lastname ,'es', full_name );
+
+// c)
+
+var ocupation = 'carpintero'
+var civil_state = 'soltero'
+var totallength = (ocupation.length + civil_state.length)
+
+console.log('La longitud de caracteres de', ocupation ,'+', civil_state ,'es', totallength );
+
+
+console.log('Fin Ejercicio 1 , comienzo Ejercicio 2 --------------------------------------');
